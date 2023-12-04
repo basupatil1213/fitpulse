cddeb7e4aa4b073bee4167592a6644fcda1ffbeb
Saurav Singh <singh.saurav@northeastern.edu>
2023-12-03 22:08:49 -0500

    Backend Basic Commit.

---
+++ FitPulseBackend/src/main/java/com/example/FitPulseBackend/FitPulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FitPulseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitPulseBackendApplication.class, args);
+	}
+
+}
---
+++ FitPulseBackend/src/test/java/com/example/FitPulseBackend/FitPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FitPulseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitPulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
