591dfd3a0bb88f7407f716e7bde6ad5e3288c12a
Basavaraj Patil <patil.ba@northeastern.edu>
2023-12-14 18:25:44 -0500

    UserParticipateController added

---
+++ CSYE6200_FP/springboot/src/main/java/com/example/controller/UserParticipateController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.common.Result;
+import com.example.entity.UserParticipate;
+import com.example.service.UserParticipateService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/userParticipate")
+public class UserParticipateController {
+
+    @Resource
+    UserParticipateService userParticipateService;
+
+    @PostMapping("/select")
+    public Result select(@RequestBody UserParticipate userParticipate) {
+        userParticipateService.select(userParticipate);
+        return Result.success();
+    }
+
+
+    @GetMapping("/selectPage")
+    public Result selectPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                             @RequestParam(defaultValue = "10") Integer pageSize,
+                             UserParticipate userParticipate) {
+        PageInfo<UserParticipate> pageInfo = userParticipateService.selectPage(pageNum, pageSize, userParticipate);
+        return Result.success(pageInfo);
+    }
+}
