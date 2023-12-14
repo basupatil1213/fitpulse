bb978c92d071707a71b824d2796dbc4e39f6348e
rohit26300 <mudundi.r@northeastern.edu>
2023-12-14 17:02:36 -0500

    feat: :sparkles: add: page divide API complete (test by postman, not connected to front-end yet)

---
+++ CSYE6200_FP/springboot/src/main/java/com/example/entity/Course.java
@@ -0,0 +1,104 @@
+package com.example.entity;
+
+public class Course {
+    private Integer id;
+    private String title;
+    private String number;
+    private String instructor;
+    private int section;
+    private String subject;
+    private String campus;
+    private int hours;
+    private String description;
+    private String location;
+    private String timetable;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public void setSection(int section) {
+        this.section = section;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(String timetable) {
+        this.timetable = timetable;
+    }
+}
---
+++ CSYE6200_FP/springboot/src/main/java/com/example/service/CourseService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import com.example.entity.Course;
+import com.example.mapper.CourseMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    @Resource
+    private CourseMapper courseMapper;
+
+    /**
+     * total: total number of search
+     * list: data list
+     * @param pageNum current page number
+     * @param pageSize size of every page
+     * @return the list of data
+     */
+
+    public PageInfo<Course> selectPage(Integer pageNum, Integer pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<Course> courseList = courseMapper.selectAll();
+        return PageInfo.of(courseList);
+    }
+}
