a4c26c42b49e4bf304b45aacfa1a99813a32cce5
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-14 18:45:10 -0500

    mappers and services added

--- CSYE6200_FP/springboot/src/main/java/com/example/mapper/AdminMapper.java
+++ CSYE6200_FP/springboot/src/main/java/com/example/mapper/AdminMapper.java
@@ -1,11 +1,34 @@
-package com.example.mapper;
+package com.example.service;
 
-import com.example.entity.Admin;
-import org.apache.ibatis.annotations.Select;
+import com.example.entity.Account;
+import com.example.exception.CustomException;
+import com.example.mapper.AdminMapper;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 
-public interface AdminMapper {
+@Service
+public class AdminService {
 
-    @Select("select * from admin where username = #{username}")
-    Admin selectByUsername(String username);
+    @Resource
+    private AdminMapper adminMapper;
+
+    /**
+     * handle login service
+     */
+    public Account login(Account account){
+        Account dbAccount = adminMapper.selectByUsername(account.getUsername());
+        System.out.println(dbAccount);
+        if (dbAccount == null) {
+            // user not fount
+            throw new CustomException(" Invalid username and/or password. ");
+        }
+        // check password
+        if (!account.getPassword().equals(dbAccount.getPassword())) {
+            throw new CustomException(" Invalid username and/or password. ");
+        }
+
+        // login successfully
+        return dbAccount;
+    }
 }
---
+++ CSYE6200_FP/springboot/src/main/java/com/example/mapper/UserParticipateMapper.java
@@ -0,0 +1,20 @@
+package com.example.mapper;
+
+import com.example.entity.UserParticipate;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface UserParticipateMapper {
+
+    @Insert("insert into user_participate (title, exercise_id, user_id) values " +
+            "(#{title}, #{exercise_id}, #{user_id})")
+    void participate(UserParticipate userParticipate);
+
+    @Select("select * from user_participate where user_id=#{user_id} and exercise_id=#{exercise_id}")
+    UserParticipate selectByCondition(UserParticipate userParticipate);
+
+    @Select("select * from user_participate where user_id like #{user_id}")
+    List<UserParticipate> selectAll(UserParticipate userParticipate);
+}
