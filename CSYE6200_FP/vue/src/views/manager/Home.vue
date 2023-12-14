98e9ba158227722cd70ba4a1866374cea61d4f6e
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-14 16:18:32 -0500

    patch fixes

--- CSYE6200_FP/vue/src/views/manager/Home.vue
+++ CSYE6200_FP/vue/src/views/manager/Home.vue
@@ -2,13 +2,13 @@
   <div>
 
     <div class="card" style="line-height: 30px">
-      <div>Welcome，<span style="color: royalblue">{{ user.username }}</span>. Hope you enjoy your day！</div>
+      <div>Welcome，{{ user.name }} Hope you enjoy your day！</div>
     </div>
 
   </div>
 </template>
 
 <script setup>
-  import request from "@/utils/request";
-  const user = JSON.parse(localStorage.getItem('student-user') || '{}')
+import request from "@/utils/request";
+const user = JSON.parse(localStorage.getItem('student-user') || '{}')
 </script>
