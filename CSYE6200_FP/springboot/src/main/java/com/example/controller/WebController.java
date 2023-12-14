b06cfe433ba9114e957036e874566a2937962af0
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-14 16:28:38 -0500

    login logic

--- CSYE6200_FP/springboot/src/main/java/com/example/controller/WebController.java
+++ CSYE6200_FP/springboot/src/main/java/com/example/controller/WebController.java
@@ -1,18 +1,32 @@
 package com.example.controller;
 
 import com.example.common.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.entity.Admin;
+import com.example.service.AdminService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @RestController
 public class WebController {
 
+    @Resource
+    private AdminService adminService;
     /**
      * Default request API
      */
     @GetMapping("/")
     public Result hello() {
         return Result.success();
+    }
+
+    /**
+     * Login request API
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody Admin admin) {
+        Admin dbAdmin = adminService.login(admin);
+        return Result.success(dbAdmin);
     }
 
 }
